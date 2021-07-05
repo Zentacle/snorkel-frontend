da1f268fd23cec4a074ed72422b13c246614488f
Matthew Betsill <matthewb22@vt.edu>
2021-07-05 11:10:05 -0400

    homepage and layout view

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,20 @@
 import Image from "next/image"
 import styles from "./Home.module.css"
-
+import SearchBar from "material-ui-search-bar"
+import React from "react";
 
 const Menu = () => {
+    const [search, setSearch] = React.useState('');
+    React.useEffect(()=>{
+        console.log(search);
+    }, [search]);
+
     return (
-        <div>hello</div>
+        <div>
+            <div className={styles.innermenu}>
+            <SearchBar className={styles.searchbar} onChange={(change) => setSearch(change)}/>
+            </div>
+        </div>
     )
 }
 
@@ -13,9 +23,11 @@ const BackImage = () => {
         <div className={styles.image}>
                 <div className={styles.imageinner}>
                 
-                <Image height='1223' width='3000' objectFit="contain" alt="here" src="/homepageimg.jpeg"/>
+                <Image className={styles.actualimage} height='1223' width='3000' objectFit="contain" alt="here" src="/homepageimg.jpeg"/>
                 </div>
+                <div className={styles.pagetitle}>Whats the viz?</div>
                 <div className={styles.menu}>
+                    
                     <Menu></Menu>
                 </div>
             </div>
