ff3d6bc571f80901cca9d3e835a81301808f56b0
Mayank Jain <mjmayank@gmail.com>
2021-07-08 19:19:06 -1000

    Fixup beach page CSS

--- src/components/BeachPage/BeachAbout/BeachAbout.js
+++ src/components/BeachPage/BeachAbout/BeachAbout.js
@@ -2,11 +2,9 @@ import styles from "../BeachAbout/BeachAbout.module.css";
 
 const BeachAbout = ({ description }) => {
     return (
-        <div className={styles.container}>
             <div className={styles.description}>
                 { description }
             </div>
-        </div>
     )
 }
 
