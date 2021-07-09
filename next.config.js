282f7c2147cee7684f86f6cc85b6b9cdb96e1856
Mayank Jain <mjmayank@gmail.com>
2021-07-09 10:09:35 -1000

    Fix image loading for review profile pics

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ module.exports = {
       'cdn.mauiguidebook.com',
       'mauiguidebook.com',
       'www.islands.com',
+      'scontent.fhnl3-1.fna.fbcdn.net',
+      'scontent.fhnl3-2.fna.fbcdn.net',
     ]
   }
 }
