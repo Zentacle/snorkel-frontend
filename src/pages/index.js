0df2f80b3c92e58017ea420a8df060943b451d5f
Mayank Jain <mjmayank@gmail.com>
2021-07-09 08:59:10 -1000

    Add social tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import Layout from '../components/Layout/Layout';
 import Home1 from '../components/Home/Home';
+import Head from 'next/head';
 
 // '/' route
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <meta property="og:title" content="DiveBriefing" key="title" />
+        <meta property="og:description" content="Search dive and snorkel spots around the world with maps, detailed reviews, and photos curated by oceans lovers like you." key="description" />
+        <meta property="og:image" content="https://divebriefing.vercel.app/social_background.png" key="image" />
+      </Head>
       <Home1>
 
       </Home1>
