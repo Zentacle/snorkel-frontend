3e2b2b064125d2fe8a21cb9516a95a26fc0dfe16
Mayank Jain <mjmayank@gmail.com>
2021-07-14 17:15:56 -0700

    Add footer links to add new locations

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -3,9 +3,11 @@ const Footer = () =>{
     return (
         <footer className={styles.footer}>
           <div className={styles.footertext}>
-         Find your next dive
-         </div>
-
+            Find your next underwater adventure
+          </div>
+          <a className={ styles.footerLink } href="mailto:mjmayank@gmail.com">Report an issue</a>
+          <a className={ styles.footerLink } href="mailto:mjmayank@gmail.com">Suggest an edit</a>
+          <a className={ styles.footerLink } href="mailto:mjmayank@gmail.com">Add a new location</a>
         </footer>
     )
 }
