ae7ca998388b07bb2784a6a6d8312387e1b92422
Mayank <63934597+mjmayank1@users.noreply.github.com>
2021-07-20 14:50:45 -1000

    Prevent review form from double submitting (#13)
    Co-authored-by: Mayank Jain <mjmayank@gmail.com>

--- src/components/PrimaryButton/index.js
+++ src/components/PrimaryButton/index.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 import styles from './styles.module.css';
 
-const PrimaryButton = ({ children, onClick }) => {
+const PrimaryButton = ({ children, disabled, onClick }) => {
   return (
-    <button onClick={onClick} className={styles.button}>
+    <button onClick={onClick} className={styles.button} disabled={disabled}>
       {children}
     </button>
   )
