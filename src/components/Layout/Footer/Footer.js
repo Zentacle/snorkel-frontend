039d7c15b053493ab2b1bf0090b6c6b60d565b77
Mayank <63934597+mjmayank1@users.noreply.github.com>
2021-07-26 08:16:07 -1000

    Add fake email signup (#18)
    Co-authored-by: Mayank Jain <mjmayank@gmail.com>

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,7 +1,21 @@
+import { sendEvent } from "hooks/amplitude"
 import styles from "./Footer.module.css"
+
+
 const Footer = () =>{
+    const onClick = () => {
+      sendEvent('submit_email');
+    }
+
     return (
         <footer className={styles.footer}>
+          <div className={ styles.emailContainer }>
+            <div>Sign up to get updates on local conditions</div>
+            <div className={ styles.formContainer }>
+              <input className={ styles.emailBox } placeholder="email"/>
+              <button className={ styles.emailSubmit } type="submit" onClick={ onClick }>Submit</button>
+            </div>
+          </div>
           <div className={styles.footertext}>
             Find your next underwater adventure
           </div>
