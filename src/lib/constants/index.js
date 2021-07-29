f2b69055c7b00f523ebd7f66e58211ce5efcc033
Matthew Betsill <matthewb22@vt.edu>
2021-07-29 16:32:47 -0400

    change rootDomain in prod

--- src/lib/constants/index.js
+++ src/lib/constants/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const rootDomain = process.env.NODE_ENV !== 'production'
   ? 'http://localhost:3000/apibackend'
   : process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? 'https://www.zentacle.com/apibackend'
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/apibackend`
+    ? 'https://www.zentacle.com/api'
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
 export const test = 'test';
