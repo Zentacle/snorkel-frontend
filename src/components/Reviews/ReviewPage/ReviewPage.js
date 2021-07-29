3b750ce6d8c866f61888192a2e54848e4cc66eff
Matthew Betsill <matthewb22@vt.edu>
2021-07-29 14:37:45 -0400

    fixes from comments, utilized photo-upload-2

--- src/components/Reviews/ReviewPage/ReviewPage.js
+++ src/components/Reviews/ReviewPage/ReviewPage.js
@@ -2,8 +2,7 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 import { useRouter } from 'next/router';
-import Cookies, { remove } from 'js-cookie'
-import Image from 'next/image';
+import Cookies from 'js-cookie'
 import { toaster } from 'evergreen-ui';
 
 import styles from "../ReviewPage/ReviewPage.module.css";
@@ -16,10 +15,7 @@ import { rootDomain } from 'lib/constants';
 import { sendEvent } from 'hooks/amplitude';
 import { useDropzone } from 'react-dropzone';
 import { MdCancel } from 'react-icons/Md';
-import { resetWarningCache } from 'prop-types';
-import { PhotoSharp } from '@material-ui/icons';
-import { get } from 'lodash';
-import { json } from 'body-parser';
+import { v4 as uuidv4 } from 'uuid';
 
 const visibilityLabel = {
     1: 'Extremely poor (<5ft)',
@@ -31,7 +27,6 @@ const visibilityLabel = {
 
 const ReviewPage = (props) => {
 
-
     const router = useRouter()
     const { beachid } = router.query
     const [activity, setActivity] = React.useState('snorkel');
@@ -39,6 +34,7 @@ const ReviewPage = (props) => {
     const [name, setName] = React.useState(props.name);
     const [text, setText] = React.useState('');
     const [visibility, setVisibility] = React.useState('');
+    //where the files are stored along with their urls
     const [fileRecords, setFileRecords] = React.useState([]);
     const [visibilityHover, setVisibilityHover] = React.useState(undefined)
     const [isSubmitDisabled, setIsSubmitDisabled] = React.useState(false);
@@ -46,7 +42,6 @@ const ReviewPage = (props) => {
     React.useEffect(() => {
 
         if (!router.isReady) return;
-
         sendEvent('review_begin', {
             'site_id': beachid,
         })
@@ -65,31 +60,23 @@ const ReviewPage = (props) => {
         }
     }, [router.isReady])
 
-
+    //creates an image for each of the urls the user has submitted
     const RenderUrls = () => {
 
-
         const removeFile = (index) => {
-
             fileRecords.splice(index, 1)
-
             setFileRecords([...fileRecords])
         }
-
-
 
         return (
             fileRecords.map(function (object, i) {
                 return (
                     <div key={object.url}>
                         <div className={styles.photooutercontainer}>
-
                             <div className={styles.photocontainer}>
                                 <div className={styles.individualphotoupload}>
                                     <div className={styles.containerdropzone}>
-
                                         <img className={styles.image} src={object.url} layout='fill' alt="pic preview" >
-
                                         </img>
                                     </div>
                                     <div className={styles.xicon} onClick={() => removeFile(i)}>
@@ -98,53 +85,34 @@ const ReviewPage = (props) => {
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 )
             })
         )
 
     }
-
-
-
-
-
-
-
+    //submission of the actual review
     const submitReview = async (body) => {
+
         setIsSubmitDisabled(true);
 
         async function uploadPhoto(file) {
-            console.log(file)
-            const filename = await encodeURIComponent(file.file.name);
-            const res = await fetch(`${rootDomain}/s3-upload`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    file: filename
-                })
-
-            });
-
+            
+            const filename = await encodeURIComponent(file.name);
+            const res = await fetch(`/apibackend/s3-upload?file=reviews/${filename}`);
             const presignedPostData = await res.json();
             const formData = new FormData();
+
             Object.keys(presignedPostData.fields).forEach(key => {
                 formData.append(key, presignedPostData.fields[key]);
             });
             // Actual file has to be appended last.
-            formData.append("file", file.file);
-            
+            formData.append("file", file);
 
-            console.log(presignedPostData.url)
-            const upload = await fetch(presignedPostData.url,{
+            const upload = await fetch(presignedPostData.url, {
                 method: 'POST',
-                body: formData
-            }
-            );
-            console.log(formData)
+                body: formData,
+            });
 
             if (upload.ok) {
                 console.log('Uploaded successfully!');
@@ -153,10 +121,10 @@ const ReviewPage = (props) => {
             }
         };
 
-
         for (let i = 0; i < fileRecords.length; i++) {
-            await uploadPhoto(fileRecords[i])
+            await uploadPhoto(fileRecords[i].file)
         }
+
         fetch(`${rootDomain}/review/add`, {
             method: 'POST',
             body: JSON.stringify(body),
@@ -196,28 +164,31 @@ const ReviewPage = (props) => {
                         </div>
                     </div>
                 }
-                <FileSubmit actualFile={acceptedFiles}></FileSubmit>
+                <FileSubmit submittedFile={acceptedFiles}></FileSubmit>
             </div>
         )
     }
-
-
-
-
-    function FileSubmit({ actualFile }) {
+    //this is called after each time the user selects a photo
+    //actualfile represents the 
+    function FileSubmit({ submittedFile }) {
 
         let f = null;
 
-        if (actualFile) {
-            actualFile.map(file => {
+        if (submittedFile) {
+            submittedFile.map(file => {
                 f = file;
             })
         }
 
         React.useEffect(() => {
 
-            if (f) {
-                const rand = (Math.random()).toString().substring(3, 10)
+            //make sure users cannot upload more than 10 photos
+            if (f && fileRecords.length == 10){
+                toaster.danger("Each review can have at most 10 photos.")
+            }
+            if (f && !(fileRecords.length > 9)) {
+                //create the uuid and place in front of file name 
+                const rand = uuidv4();
                 let testid = rand + f.path
                 const myNewFile = new File([f], testid, { type: f.type });
                 const objUrl = URL.createObjectURL(myNewFile)
@@ -229,14 +200,10 @@ const ReviewPage = (props) => {
                 fileRecordsCopy.push(newFileRecord)
                 setFileRecords([...fileRecordsCopy])
 
-
             }
+            
 
-        }, [f]);
-
-
-
-
+        }, [submittedFile]);
 
         return (<div />)
 
@@ -275,19 +242,16 @@ const ReviewPage = (props) => {
                     </div>
                     <div className={styles.photoscontainer}>
                         <div className={styles.photooutercontainer}>
-                            <div className={styles.photocontainer}>
-                                <div className={styles.individualphotoupload}>
-                                    <div className={styles.containerdropzone}>
-                                        <DropZoneArea></DropZoneArea>
-                                        <br />
-                                    </div>
+                            <div className={styles.individualphotoupload}>
+                                <div className={styles.containerdropzone}>
+                                    <DropZoneArea></DropZoneArea>
+                                    <br />
                                 </div>
                             </div>
                         </div>
                         <RenderUrls></RenderUrls>
                     </div>
                 </div>
-
                 <PrimaryButton className={styles.nextbutton} disabled={isSubmitDisabled} onClick={() => submitReview({
                     'activity_type': activity,
                     rating,
@@ -301,7 +265,5 @@ const ReviewPage = (props) => {
         </Layout>
     )
 }
-
-
 
 export default ReviewPage;
