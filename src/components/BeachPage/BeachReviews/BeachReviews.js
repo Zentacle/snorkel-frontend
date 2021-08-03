fa2c6c72b32bb8e72eb772cb11ec72a9fd297493
Mayank <63934597+mjmayank1@users.noreply.github.com>
2021-08-02 15:29:59 -1000

    Add virtualized list component for reviews (#22)
    Co-authored-by: Mayank Jain <mjmayank@gmail.com>

--- src/components/BeachPage/BeachReviews/BeachReviews.js
+++ src/components/BeachPage/BeachReviews/BeachReviews.js
@@ -1,11 +1,23 @@
 import React from "react";
+import {
+    AutoSizer,
+    WindowScroller,
+    List,
+    CellMeasurerCache,
+    CellMeasurer,
+} from 'react-virtualized';
 
 import styles from "../BeachReviews/BeachReviews.module.css";
 import IndividualReview from "./IndividualReview/IndividualReview";
 import { useCurrentUser } from 'context/usercontext';
 import { PrimaryLink } from 'components/PrimaryButton';
 import { sendEvent } from 'hooks/amplitude';
-  
+
+const cache = new CellMeasurerCache({
+    defaultHeight: 150,
+    fixedWidth: true,
+});
+
 const BeachReviews = (props) => {
     const { beachid } = props;
     const [reviews, setReviews] = React.useState(props.reviews);
@@ -27,7 +39,38 @@ const BeachReviews = (props) => {
                 <PrimaryLink onClick={ onReviewClick } className={styles.reviewbutton} href={ link }>Write a Review</PrimaryLink>
             </div>
             { reviews && reviews.length
-                ? reviews.map(review => <IndividualReview key={ review.id } review={review} user={review.user}></IndividualReview>)
+                ? <WindowScroller>
+                    {({ height, isScrolling, onChildScroll, scrollTop }) => (
+                    <AutoSizer disableHeight>
+                        {({width}) => <List
+                            autoHeight
+                            height={height}
+                            isScrolling={isScrolling}
+                            onScroll={onChildScroll}
+                            scrollTop={scrollTop}
+                            rowCount={reviews.length}
+                            deferredMeasurementCache={cache}
+                            rowHeight={cache.rowHeight}
+                            width={width}
+                            rowRenderer={({ index, parent, style }) => (
+                                <CellMeasurer
+                                    cache={cache}
+                                    columnIndex={0}
+                                    key={reviews[index].id}
+                                    parent={parent}
+                                    rowIndex={index}
+                                >
+                                    <IndividualReview
+                                        key={ reviews[index].id }
+                                        review={reviews[index]}
+                                        user={reviews[index].user}
+                                        style={style}
+                                    />
+                                </CellMeasurer>
+                            )}
+                        />}
+                    </AutoSizer>)}
+                </WindowScroller>
                 : <div className={ styles.emptyState }>No reviews yet. Be the first!</div>
             }
         </div>
