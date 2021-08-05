26b684971e9b4382a529d63d45e2627512e921ad
Mayank Jain <mjmayank@gmail.com>
2021-08-05 00:40:31 -0400

    Add delay for google sign in to ensure it has loaded

--- src/components/CreateAccount/CreateAccount.js
+++ src/components/CreateAccount/CreateAccount.js
@@ -66,31 +66,25 @@ const CreateAccount = () => {
         }
 
         const initializeGSI = () => {
-            google.accounts.id.initialize({
-                client_id: '609299692665-bl3secuu5i4v1iumjm0kje0db1lge1ec.apps.googleusercontent.com',
-                callback: handleLogin,
-            });
-            google.accounts.id.prompt(notification => {
-                if (notification.isNotDisplayed()) {
-                    console.log(notification.getNotDisplayedReason())
-                } else if (notification.isSkippedMoment()) {
-                    console.log(notification.getSkippedReason())
-                } else if(notification.isDismissedMoment()) {
-                    console.log(notification.getDismissedReason())
-                }
-                console.log(notification)
-                fetch(`${rootDomain}/user/google_register`, {
-                    method: 'POST',
-                    body: notification,
-                    headers: {
-                        'Content-Type': 'application/json'
+            if (google) {
+                google.accounts.id.initialize({
+                    client_id: '609299692665-bl3secuu5i4v1iumjm0kje0db1lge1ec.apps.googleusercontent.com',
+                    callback: handleLogin,
+                });
+                google.accounts.id.prompt(notification => {
+                    if (notification.isNotDisplayed()) {
+                        console.log(notification.getNotDisplayedReason())
+                    } else if (notification.isSkippedMoment()) {
+                        console.log(notification.getSkippedReason())
+                    } else if(notification.isDismissedMoment()) {
+                        console.log(notification.getDismissedReason())
                     }
-                })
-            });
+                });
+            }
         }
         const script = document.createElement('script')
         script.src = 'https://accounts.google.com/gsi/client'
-        script.onload = initializeGSI()
+        script.onload = setTimeout(initializeGSI, 3000)
         script.async = true;
         document.querySelector('body').appendChild(script)
     }, [])
