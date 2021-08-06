961a4eefc3fd5c4f265b91863bd533333e7220bc
Matthew Betsill <matthewb22@vt.edu>
2021-08-06 14:16:41 -0400

    fix undefined error in beach page

--- src/components/BeachPage/BeachPage.js
+++ src/components/BeachPage/BeachPage.js
@@ -15,7 +15,7 @@ const BackImage = (props) => {
     const router = useRouter();
     const [photosHeight, setPhotosHeight] = React.useState(0);
     const [photoArray, setPhotoArray] = React.useState([]);
-    
+
     const changeHeight = () => {
         if (photosHeight == 0) {
             setPhotosHeight(200)
@@ -49,33 +49,34 @@ const BackImage = (props) => {
     const [settings2, setSettings2] = React.useState(null);
     const [settings3, setSettings3] = React.useState(null);
     React.useEffect(() => {
-        
+
         if (photoArray.length > 0) {
             const arLen = photoArray.length;
             setSettings1({
                 width: '100px',
                 height: ['100px'],
                 layout: [1],
-                photos: [{ source: photoArray[arLen-1].signedurl.data }],
+                photos: [{ source: photoArray[arLen - 1].signedurl.data }],
                 showNumOfRemainingPhotos: false
             })
-
-            setSettings2({
-                width: '100px',
-                height: ['100px'],
-                layout: [1],
-                photos: [{ source: photoArray[arLen-2].signedurl.data }],
-                showNumOfRemainingPhotos: false
-            })
-
-            setSettings3({
-                width: '100px',
-                height: ['100px'],
-                layout: [1],
-                photos: [{ source: photoArray[arLen-3].signedurl.data }],
-                showNumOfRemainingPhotos: false
-            })
-
+            if (photoArray.length > 1) {
+                setSettings2({
+                    width: '100px',
+                    height: ['100px'],
+                    layout: [1],
+                    photos: [{ source: photoArray[arLen - 2].signedurl.data }],
+                    showNumOfRemainingPhotos: false
+                })
+            }
+            if (photoArray.length > 2) {
+                setSettings3({
+                    width: '100px',
+                    height: ['100px'],
+                    layout: [1],
+                    photos: [{ source: photoArray[arLen - 3].signedurl.data }],
+                    showNumOfRemainingPhotos: false
+                })
+            }
         }
 
     }, [photoArray])
