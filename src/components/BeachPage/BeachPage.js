d21fefb83b2162c83b08ab55b10ea844dacbf176
Matthew Betsill <matthewb22@vt.edu>
2021-08-10 12:30:41 -0400

    test push

--- src/components/BeachPage/BeachPage.js
+++ src/components/BeachPage/BeachPage.js
@@ -24,7 +24,7 @@ const BackImage = (props) => {
                 setPhotosHeight(100)
             }
         } else {
-            setPhotosHeight(0)
+            setPhotosHeight(1)
         }
     }
 
