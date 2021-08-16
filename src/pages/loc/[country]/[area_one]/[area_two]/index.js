582367b60125bf8a5bf5f60cc423dcecb6174839
Mayank Jain <mjmayank@gmail.com>
2021-08-16 10:43:46 -0400

    Statically generate location pages

--- src/pages/loc/[country]/[area_one]/[area_two]/index.js
+++ src/pages/loc/[country]/[area_one]/[area_two]/index.js
@@ -1,10 +1,10 @@
 import Page from 'pages/index';
 import { rootDomain } from "src/lib/constants";
 
-export async function getServerSideProps(context) {
-  const area_two = context.query.area_two;
-  const area_one = context.query.area_one;
-  const country = context.query.country;
+export async function getStaticProps(context) {
+  const area_two = context.params.area_two;
+  const area_one = context.params.area_one;
+  const country = context.params.country;
   const sorts = ['top', 'latest', 'default']
   const props = {};
   await Promise.all(sorts.map(async sort => {
@@ -28,6 +28,22 @@ export async function getServerSideProps(context) {
 
   return {
     props, // will be passed to the page component as props
+    revalidate: 3600,
+  }
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${rootDomain}/locality/area_two`)
+  const data = await res.json()
+  return {
+      paths: data.data.map(loc => ({
+          params: {
+              country: loc.country.short_name,
+              area_one: loc.area_one.short_name,
+              area_two: loc.short_name,
+          }
+      })),
+      fallback: 'blocking',
   }
 }
 
