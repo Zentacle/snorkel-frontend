0571c9a30427e3cadcd5a88f798c948910807694
Mayank Jain <mjmayank@gmail.com>
2021-09-09 14:33:45 -1000

    Fix nearby beaches carousel

--- src/pages/Beach/[[...slug]]/index.js
+++ src/pages/Beach/[[...slug]]/index.js
@@ -80,7 +80,7 @@ const Beach = (props) => {
         }).then(res =>
             res.json()
         ).then(beach_data => {
-            //setNearbyBeaches([...beach_data.data])
+            setNearbyBeaches([...beach_data.data])
         });
     }, [router.isReady, router.query.slug])
 
