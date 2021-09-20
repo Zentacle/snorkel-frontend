871d6305af86f3337bbb9a41ec3c64517b927bee
Mayank Jain <mjmayank@gmail.com>
2021-09-20 11:09:21 -1000

    Update breadcrumb logic

--- src/components/Breadcrumbs/index.js
+++ src/components/Breadcrumbs/index.js
@@ -5,14 +5,14 @@ import styles from './styles.module.css';
 const Breadcrumbs = ({country, area_one, area_two}) => {
   return (
     country && country.short_name ? <div className={styles.locBreadcrumbContainer}>
-        <Link href={`/loc/${country.short_name}`}>
+        <Link href={country.url}>
             <a className={styles.locBreadcrumb}>
                 {country.name}
             </a>
         </Link>
         {area_one && area_one.short_name && <>
             <span className={styles.locBreadcrumb}>›</span>
-            <Link href={`/loc/${country.short_name}/${area_one.short_name}`}>
+            <Link href={area_one.url}>
                 <a className={styles.locBreadcrumb}>
                     {area_one.name}
                 </a>
@@ -21,7 +21,7 @@ const Breadcrumbs = ({country, area_one, area_two}) => {
         }
         {area_two && area_two.short_name && <>
             <span className={styles.locBreadcrumb}>›</span>
-            <Link href={`/loc/${country.short_name}/${area_one.short_name}/${area_two.short_name}`}>
+            <Link href={area_two.url}>
                 <a className={styles.locBreadcrumb}>
                     {area_two.name}
                 </a>
