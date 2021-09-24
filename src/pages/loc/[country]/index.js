349951a8338e4e71df6470cf1c5c0f82d01dc067
Mayank Jain <mjmayank@gmail.com>
2021-09-24 07:46:09 -1000

    Add patrons to location pages

--- src/pages/loc/[country]/index.js
+++ src/pages/loc/[country]/index.js
@@ -115,6 +115,19 @@ const Home = (props) => {
           <div className={styles.marginContainer}>
             {props.area.description}
           </div>
+          {
+              area.area_two && area.area_two.id == 2 && <div>
+                  <h3 className={styles.sectionTitle}>{name} Patrons</h3>
+                  <div className={styles.description}>
+                      <a
+                          onClick={ () => { ga.event({ action: "purchase", params: { eventLabel: 'Kona Shore Divers' } })} }
+                          className={styles.patronName}
+                          href="https://www.konashoredivers.com"
+                      >Kone Shore Divers</a>
+                      <span> - Our goal is to give you the opportunity to share and explore the underwater world in and around the near shore waters of Kailua-Kona. We focus on small group sizes and strive to offer the best personalized service we can to our guests. From the first timer to the old timer come with us and see what the Kona Shore has to offer!</span>
+                  </div>
+              </div>
+          }
           <div>
             {
               props.default.map(location => (
