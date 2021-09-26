d6c66b8751ddf391ab8d78e97f3d9c02824d6fc7
Mayank Jain <mjmayank@gmail.com>
2021-09-25 19:19:26 -1000

    Lowercase search term in analytics

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -6,35 +6,35 @@ import { sendEvent } from 'hooks/amplitude';
 import SearchIcon from '@material-ui/icons/Search';
 
 const SearchBar = (props) => {
-  const router = useRouter();
-  const [search, setSearch] = React.useState(props.value || '');
-  const largeSearchBar = props.largeSearchBar;
-  const barSize = largeSearchBar ? styles.lgBar : ''; 
-    
-  const conductSearch = () => {
-    sendEvent('submit_search', {
-        'query': search,
-    });
-    router.push(`/search?search_term=${search}`)
-}
+    const router = useRouter();
+    const [search, setSearch] = React.useState(props.value || '');
+    const largeSearchBar = props.largeSearchBar;
+    const barSize = largeSearchBar ? styles.lgBar : '';
 
-  return (
-<div className={ `${barSize} ${styles.container}` }>
-            <div className={ styles.inputContainer }>
+    const conductSearch = () => {
+        sendEvent('submit_search', {
+            'query': search.toLowerCase(),
+        });
+        router.push(`/search?search_term=${search}`)
+    }
+
+    return (
+        <div className={`${barSize} ${styles.container}`}>
+            <div className={styles.inputContainer}>
                 <input
                     placeholder="Search"
                     value={search}
                     className={styles.searchbar}
-                    onChange={ e => setSearch(e.target.value) }
-                    onKeyPress={ (e) => {
+                    onChange={e => setSearch(e.target.value)}
+                    onKeyPress={(e) => {
                         if (e.key === 'Enter') {
                             conductSearch()
                         }
                     }}
                 />
             </div>
-            <button className={ `${barSize} ${styles.searchButton}` } onClick={ conductSearch }>
-                <SearchIcon className={ styles.icon }/>
+            <button className={`${barSize} ${styles.searchButton}`} onClick={conductSearch}>
+                <SearchIcon className={styles.icon} />
             </button>
         </div>
     )
