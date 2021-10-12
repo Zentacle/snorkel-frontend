042264ac691bb238706410a3f9b570abcce09376
Mayank Jain <mjmayank@gmail.com>
2021-10-12 10:09:39 -0700

    Fix breadcrumb item

--- src/components/Breadcrumbs/index.js
+++ src/components/Breadcrumbs/index.js
@@ -6,37 +6,43 @@ const Breadcrumbs = ({ country, area_one, area_two }) => {
     return (
         <div className={styles.locBreadcrumbContainer}>
             <div itemScope={true} itemType="http://schema.org/BreadcrumbList">
-                {country && country.short_name && <Link href={country.url}>
-                    <a className={styles.locBreadcrumb} itemProp="itemListElement" itemScope={true} itemType="http://schema.org/ListItem">
-                        <span itemProp="name">{country.name}</span>
-                        <meta itemProp="position" content="1" />
-                    </a>
-                </Link>}
-                {area_one && area_one.short_name && <>
-                    <span className={styles.locBreadcrumb}>›</span>
-                    <Link href={area_one.url}>
-                        <a className={styles.locBreadcrumb}  itemProp="itemListElement" itemScope={true} itemType="http://schema.org/ListItem">
-                            <span itemProp="name">{area_one.name}</span>
-                            <meta itemProp="position" content="2" />
-                        </a>
-                    </Link>
-                </>
+                {country && country.short_name &&
+                    <span itemProp="itemListElement" itemScope={true} itemType="http://schema.org/ListItem">
+                        <Link href={country.url}>
+                            <a className={styles.locBreadcrumb} itemProp="item">
+                                <span itemProp="name">{country.name}</span>
+                                <meta itemProp="position" content="1" />
+                            </a>
+                        </Link>
+                    </span>
                 }
-                {area_two && area_two.short_name && <>
-                    <span className={styles.locBreadcrumb}>›</span>
-                    <Link href={area_two.url}>
-                        <a className={styles.locBreadcrumb}  itemProp="itemListElement" itemScope={true} itemType="http://schema.org/ListItem">
-                            <span itemProp="name">{area_two.name}</span>
-                            <meta itemProp="position" content="3" />
-                        </a>
-                    </Link>
-                </>
+                {area_one && area_one.short_name &&
+                    <span itemProp="itemListElement" itemScope={true} itemType="http://schema.org/ListItem">
+                        <span className={styles.locBreadcrumb}>›</span>
+                        <Link href={area_one.url}>
+                            <a className={styles.locBreadcrumb} itemProp="item">
+                                <span itemProp="name">{area_one.name}</span>
+                                <meta itemProp="position" content="2" />
+                            </a>
+                        </Link>
+                    </span>
+                }
+                {area_two && area_two.short_name &&
+                    <span itemProp="itemListElement" itemScope={true} itemType="http://schema.org/ListItem">
+                        <span className={styles.locBreadcrumb}>›</span>
+                        <Link href={area_two.url}>
+                            <a className={styles.locBreadcrumb} itemProp="item">
+                                <span itemProp="name">{area_two.name}</span>
+                                <meta itemProp="position" content="3" />
+                            </a>
+                        </Link>
+                    </span>
                 }
             </div>
             <div className={styles.search}>
                 <SearchBar largeSearchBar={false}></SearchBar>
             </div>
-        </div>
+        </div >
     )
 }
 
