78d407f8e9dab5e44aa28276494626c8f5ce64ca
Mayank Jain <mjmayank@gmail.com>
2021-11-02 11:57:47 -1000

    Return 301 for shore diving pages

--- src/pages/Earth/[region]/[destination]/[site]/index.htm/index.js
+++ src/pages/Earth/[region]/[destination]/[site]/index.htm/index.js
@@ -2,7 +2,7 @@ import Beach from 'pages/Beach/[[...slug]]'
 import { rootDomain } from 'lib/constants';
 
 export async function getStaticProps(context) {
-    const startTime = Date.now();
+    // const startTime = Date.now();
     const region = context.params.region;
     const destination = context.params.destination;
     const site = context.params.site;
@@ -13,53 +13,52 @@ export async function getStaticProps(context) {
         }
     })
     const beach_data = await res.json()
-
-    const beach_id = beach_data.data.id;
-    const response = await fetch(`${rootDomain}/review/get?beach_id=${beach_id}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    const review_data = await response.json();
-
-    const nearbyBeaches = await fetch(`${rootDomain}/spots/nearby?beach_id=${beach_id}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(res =>
-        res.json()
-    ).then(beach_data => {
-        if (beach_data.data) {
-            return beach_data.data;
-        }
-    });
-
     if (!beach_data) {
         return {
             notFound: true,
         }
     }
 
-    console.log(`beach_api_timing: ${Date.now() - startTime}ms`)
-    return {
-        props: {
-            'beach': beach_data.data,
-            'reviews': review_data.data,
-            'tides': [],
-            'isShorediving': true,
-            'nearbyBeaches': nearbyBeaches,
-        }, // will be passed to the page component as props
-        revalidate: 10,
-    }
-
-    // return {
-    //     redirect: {
-    //         destination: beach_data.data.url,
-    //         permanent: false,
+    // const beach_id = beach_data.data.id;
+    // const response = await fetch(`${rootDomain}/review/get?beach_id=${beach_id}`, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json'
     //     }
+    // })
+    // const review_data = await response.json();
+
+    // const nearbyBeaches = await fetch(`${rootDomain}/spots/nearby?beach_id=${beach_id}`, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // }).then(res =>
+    //     res.json()
+    // ).then(beach_data => {
+    //     if (beach_data.data) {
+    //         return beach_data.data;
+    //     }
+    // });
+
+    // console.log(`beach_api_timing: ${Date.now() - startTime}ms`)
+    // return {
+    //     props: {
+    //         'beach': beach_data.data,
+    //         'reviews': review_data.data,
+    //         'tides': [],
+    //         'isShorediving': true,
+    //         'nearbyBeaches': nearbyBeaches,
+    //     }, // will be passed to the page component as props
+    //     revalidate: 10,
     // }
+
+    return {
+        redirect: {
+            destination: beach_data.data.url,
+            permanent: true,
+        }
+    }
 }
 
 export async function getStaticPaths() {
