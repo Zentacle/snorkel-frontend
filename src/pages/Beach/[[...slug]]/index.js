d64c85c8a2f05e64bdb1a94d525f7700d1275cd8
Mayank Jain <mjmayank@gmail.com>
2022-04-10 14:34:36 -0400

    Add geocoding on page load

--- src/pages/Beach/[[...slug]]/index.js
+++ src/pages/Beach/[[...slug]]/index.js
@@ -40,6 +40,10 @@ export async function getStaticProps(context) {
 
     const beach_data = await res.json()
 
+    if (!beach_data.country) {
+        fetch(`${rootDomain}/spots/geocode?id=${beachid}`)
+    }
+
     if (`/Beach/${beachid}/${beachNameFromURL}` != beach_data.data.url) {
         return {
             redirect: {
