d28a052cfb583a1ad0216036793dc8be050a579d
Mayank Jain <mjmayank@gmail.com>
2022-04-20 23:28:53 -0400

    add event listener for sign in with apple success

--- src/hooks/useAppleButton.js
+++ src/hooks/useAppleButton.js
@@ -3,38 +3,49 @@ import { clientSideDomain } from "src/lib/constants";
 import * as ga from 'lib/ga';
 
 const useGoogleButton = (redirectURL, user) => () => {
-  if (!user) { return }
-  if (user && user.id) { return }
-  const handleLogin = (response) => {
-    if (response.credential) {
-      fetch(`${clientSideDomain}/user/google_register`, {
-        method: 'POST',
-        body: JSON.stringify(response),
-        headers: {
-          'Content-Type': 'application/json'
+    if (!user) { return }
+    if (user && user.id) { return }
+    const handleLogin = (response) => {
+        if (response.credential) {
+            fetch(`${clientSideDomain}/user/google_register`, {
+                method: 'POST',
+                body: JSON.stringify(response),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(() => {
+                sendEvent('google_register_success');
+                ga.event({
+                    action: "sign_up",
+                    params: {
+                        'method': "Google Button"
+                    }
+                })
+                window.location.href = redirectURL
+            })
+        } else {
+            sendEvent('google_register_error');
         }
-      }).then(() => {
-        sendEvent('google_register_success');
-        ga.event({
-          action: "sign_up",
-          params: {
-            'method': "Google Button"
-          }
-        })
-        window.location.href = redirectURL
-      })
-    } else {
-      sendEvent('google_register_error');
     }
-  }
 
-  const initializeGSI = () => {
-  }
-  const script = document.createElement('script')
-  script.src = 'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js'
-  script.onload = setTimeout(initializeGSI, 1000)
-  script.async = true;
-  document.querySelector('body').appendChild(script)
+    const initializeGSI = () => {
+        // Listen for authorization success.
+        document.addEventListener('AppleIDSignInOnSuccess', (event) => {
+            // Handle successful response.
+            console.log(event.detail.data);
+        });
+
+        // Listen for authorization failures.
+        document.addEventListener('AppleIDSignInOnFailure', (event) => {
+            // Handle error.
+            console.log(event.detail.error);
+        });
+    }
+    const script = document.createElement('script')
+    script.src = 'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js'
+    script.onload = setTimeout(initializeGSI, 1000)
+    script.async = true;
+    document.querySelector('body').appendChild(script)
 }
 
 
