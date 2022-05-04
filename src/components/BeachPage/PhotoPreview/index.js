fc874bf13a9ec0780f07dbacc5c1f0e3a2c505ea
Mayank Jain <mjmayank@gmail.com>
2022-05-04 13:17:03 -0400

    Fix loading of images on beach page

--- src/components/BeachPage/PhotoPreview/index.js
+++ src/components/BeachPage/PhotoPreview/index.js
@@ -10,32 +10,30 @@ const PhotoPreview = (props) => {
   const [settings, setSettings] = React.useState(null);
 
   React.useEffect(() => {
-      if (photoArray.length > 0) {
-          setSettings({
-              width: '320px',
-              height: ['100px', '100px', '100px'],
-              layout: [Math.min(3, photoArray.length)],
-              photos: photoArray.map(photo => ({ source: photo.signedurl })),
-              showNumOfRemainingPhotos: true
-          })
-      }
+    if (photoArray.length > 0) {
+      setSettings({
+        width: '320px',
+        height: ['100px', '100px', '100px'],
+        layout: [Math.min(3, photoArray.length)],
+        photos: photoArray.map(photo => ({ source: photo.signedurl })),
+        showNumOfRemainingPhotos: true
+      })
+    }
 
   }, [photoArray])
 
   React.useEffect(() => {
-    if (!props.name) {
-      fetch(`${rootDomain}/beachimages?beach_id=${props.beach.id}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(response => {
-        return response.json();
-      }).then(data => {
-        setPhotoArray([...data.data]);
-      })
-    }
-  }, [])
+    fetch(`${rootDomain}/beachimages?beach_id=${props.beach.id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      return response.json();
+    }).then(data => {
+      setPhotoArray([...data.data]);
+    })
+  }, [props.beach.id])
 
   return (
     settings && settings.photos && settings.photos[0].source ? <div className={styles.photocontainer}>
