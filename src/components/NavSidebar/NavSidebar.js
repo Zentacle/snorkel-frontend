b2e48adf6bbe1c52a04feea2c2045c01c8e7c6ad
Mayank Jain <mjmayank@gmail.com>
2022-05-10 15:15:15 -0400

    Remove some unused code

--- src/components/NavSidebar/NavSidebar.js
+++
@@ -1,8 +0,0 @@
-import styles from "./NavSidebar.module.css";
-const NavSidebar = () =>{
-    return(
-        <div className={styles.outersidebar}>sidebar</div>
-    )
-}
-
-export default NavSidebar;
