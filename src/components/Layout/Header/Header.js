45e426b825b94460d40059e7e072a9f56cbc0289
Mayank Jain <mjmayank@gmail.com>
2022-05-15 23:24:45 -0400

    Update prefetches for header

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -9,7 +9,7 @@ import { useRouter } from "next/router";
 const Profile = ({ user }) => {
     if (user) {
         return (
-            <Link href={`/user/${user.username}`}>
+            <Link prefetch={false} href={`/user/${user.username}`}>
                 <a className={styles.outerprofile}>
                     {user.username}
                 </a>
@@ -19,19 +19,19 @@ const Profile = ({ user }) => {
     else return null;
 }
 
-const Logo = ({isShorediving=false}) => {
+const Logo = ({ isShorediving = false }) => {
     return isShorediving
         ? (
             <a href='https://www.shorediving.com' className={styles.headertitlelink}>
                 <Image src='/sdlogo.gif' height='32' width='78' alt="Shore Diving logo" />
             </a>
         )
-        : (<Link href='/'>
-                <a className={styles.headertitlelink}>
-                    <Image src='/logo.png' height='32' width='32' alt="Zentacle logo" />
-                    <span className={styles.headertitle}>Zentacle</span>
-                </a>
-            </Link>
+        : (<Link prefetch={false} href='/'>
+            <a className={styles.headertitlelink}>
+                <Image src='/logo.png' height='32' width='32' alt="Zentacle logo" />
+                <span className={styles.headertitle}>Zentacle</span>
+            </a>
+        </Link>
         )
 }
 
@@ -46,19 +46,19 @@ const Header = (props) => {
         <div className={styles.header}>
             <div className={styles.headercontainer}>
                 <div className={styles.headerbutton}>
-                    <Logo isShorediving={isShorediving}/>
+                    <Logo isShorediving={isShorediving} />
                 </div>
                 <div className={styles.spaceholder}>
                     <div>
-                        {currentUser && currentUser.id && <Link href="/add/spot">
+                        {currentUser && currentUser.id && <Link prefetch={false} href="/add/spot">
                             <a className={styles.addSpot}>Add New Spot</a>
                         </Link>}
                     </div>
-                    {currentUser && currentUser.id && <ProfilePic user={currentUser} size={32}/> }
+                    {currentUser && currentUser.id && <ProfilePic user={currentUser} size={32} />}
                     <div className={styles.rightButton}>
                         {currentUser && currentUser.id
                             ? <Profile user={currentUser}></Profile>
-                            : <Link href='/register'>
+                            : <Link prefetch={false} href='/register'>
                                 <a className={styles.loginbutton}>Create Account</a>
                             </Link>
                         }
