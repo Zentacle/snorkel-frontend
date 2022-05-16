a4744b38c3dc4cddc13b814e1b110e1ed1f22ee2
Mayank Jain <mjmayank@gmail.com>
2022-05-16 13:06:29 -0400

    Fix issues with svg

--- src/pages/Beach/[beachid]/[beach_name]/photos/index.js
+++ src/pages/Beach/[beachid]/[beach_name]/photos/index.js
@@ -63,7 +63,7 @@ const PhotoPage = (props) => {
                 <Link href={url}>
                     <a className={styles.backTo}>
                         <svg className={styles.backIcon} width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7.13069 12.7147L1.61035 7.00042L7.13069 1.28613" stroke="#0B0B0B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M7.13069 12.7147L1.61035 7.00042L7.13069 1.28613" stroke="#0B0B0B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
 
                         Back to site details
