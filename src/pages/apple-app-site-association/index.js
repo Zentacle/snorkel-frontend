4435590b4fecb9e64ea08b18453f6ccb251bea10
Mayank Jain <mjmayank@gmail.com>
2022-05-16 17:58:38 -0400

    Update apple site association

---
+++ src/pages/apple-app-site-association/index.js
@@ -0,0 +1,15 @@
+const AppleSite = () => {
+  return `{
+    "applinks": {
+      "apps": [],
+      "details": [
+        {
+          "appID": "1611242564.org.reactjs.native.example.Zentacle",
+          "paths": ["/Beach/*/*"]
+        }
+      ]
+    }
+  }`
+}
+
+export default AppleSite;
