41936ba1aa3c64f06680c2c2d0f86b3b815c2b37
Mayank Jain <mjmayank@gmail.com>
2022-06-23 11:07:17 -0400

    Show email banner less

--- src/components/EmailBanner/index.tsx
+++ src/components/EmailBanner/index.tsx
@@ -160,7 +160,7 @@ const EmailBannerContainer = () => {
     const isLoggedOut = !currentUser.id;
     const shouldShowBanner = (never_seen_banner || !did_close_in_last_day) && isLoggedOut;
     if (shouldShowBanner) {
-      setTimeout(() => setIsShown(true), 5000);
+      setTimeout(() => setIsShown(true), 30000);
     } else {
       sendEvent('bottom_banner_suppress');
     }
