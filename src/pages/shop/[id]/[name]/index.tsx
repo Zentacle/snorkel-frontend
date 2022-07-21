13eec2352edd559a541100e0abc39ee39bd3c6c1
Matthew Betsill <matthewb22@vt.edu>
2022-07-21 08:54:46 -0400

    added shop page and layout

---
+++ src/pages/shop/[id]/[name]/index.tsx
@@ -0,0 +1,13 @@
+import Layout from "components/Layout/Layout";
+
+function Shop(props: any){
+   return ( 
+   <Layout isShorediving={props.isShorediving}>
+        <div>
+            hello
+        </div>
+    </Layout>
+   )
+}
+
+export default Shop;
