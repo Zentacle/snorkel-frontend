63c3e5d12ff26bea7f903c45245946e690804468
Mayank Jain <mjmayank@gmail.com>
2022-07-22 10:37:37 -0400

    Add tracking to 404/500 pages

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,14 +1,28 @@
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 import Layout from "components/Layout/Layout";
+import { sendEvent } from 'hooks/amplitude';
+import { useCurrentUser } from 'context/usercontext';
 import styles from './styles.module.css';
 
 export default function Custom404() {
+  let { state } = useCurrentUser();
+  const currentUser = state.user;
+
+  useEffect(() => {
+    if (currentUser) {
+      sendEvent('page_view', {
+        type: '404',
+      });
+    }
+  }, [currentUser])
+
   return <Layout>
     <div className={styles.container}>
-        <h1>Looking for directions? We couldn&apos;t find that page (404)</h1>
-        <h6>Try going back to the home page</h6>
-        <Image src='/turtle.jpg' height={455} width={340} />
+      <h1>Looking for directions? We couldn&apos;t find that page (404)</h1>
+      <h6>Try going back to the home page</h6>
+      <Image src='/turtle.jpg' height={455} width={340} />
     </div>
   </Layout>
 }
--- src/pages/500/index.tsx
+++ src/pages/500/index.tsx
@@ -1,9 +1,23 @@
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 import Layout from "components/Layout/Layout";
+import { sendEvent } from 'hooks/amplitude';
+import { useCurrentUser } from 'context/usercontext';
 import styles from './styles.module.css';
 
-export default function Custom404() {
+export default function Custom500() {
+  let { state } = useCurrentUser();
+  const currentUser = state.user;
+
+  useEffect(() => {
+    if (currentUser) {
+      sendEvent('page_view', {
+        type: '500',
+      });
+    }
+  }, [currentUser])
+
   return <Layout>
     <div className={styles.container}>
         <h1>Hmm, something went wrong (500)</h1>
