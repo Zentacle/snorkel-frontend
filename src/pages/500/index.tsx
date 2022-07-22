0e891912ec80fece5a302485087be3026179c890
Mayank Jain <mjmayank@gmail.com>
2022-07-22 09:38:43 -0400

    Add error code and create 500 page

--- src/pages/500/index.tsx
+++ src/pages/500/index.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.css';
 export default function Custom404() {
   return <Layout>
     <div className={styles.container}>
-        <h1>Looking for directions? We couldn't find that page (404)</h1>
+        <h1>Hmm, something went wrong (500)</h1>
         <h6>Try going back to the home page</h6>
         <Image src='/turtle.jpg' height={455} width={340} />
     </div>
