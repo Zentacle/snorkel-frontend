cea899ed8ada7358d9b208f21ba6cd4b8ab6b76b
Mayank Jain <mjmayank@gmail.com>
2022-07-22 10:23:41 -0400

    Fix apostraphe issue

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.css';
 export default function Custom404() {
   return <Layout>
     <div className={styles.container}>
-        <h1>Looking for directions? We couldn't find that page (404)</h1>
+        <h1>Looking for directions? We couldn&apos;t find that page (404)</h1>
         <h6>Try going back to the home page</h6>
         <Image src='/turtle.jpg' height={455} width={340} />
     </div>
