4a445a8866b7f2d61640bd1537aa85b7f4a73ce3
Mayank Jain <mjmayank@gmail.com>
2023-01-07 17:19:14 -0500

    Fix production by removing amplitude events

--- src/hooks/amplitude.js
+++ src/hooks/amplitude.js
@@ -5,22 +5,22 @@ export const initAmplitude = (userId) => {
   if (process.browser) {
     amplitude = require('amplitude-js');
     if (userId) {
-      amplitude.getInstance().init('b628782272a6f671076be09a7b6fb6b7', userId=userId);
+      // amplitude.getInstance().init('b628782272a6f671076be09a7b6fb6b7', userId=userId);
     } else {
-      amplitude.getInstance().init('b628782272a6f671076be09a7b6fb6b7');
+      // amplitude.getInstance().init('b628782272a6f671076be09a7b6fb6b7');
     }
-    amplitude.getInstance().logEvent('view_app');
+    // amplitude.getInstance().logEvent('view_app');
   }
 };
 
 export const sendEvent = (event, obj) => {
   amplitude = require('amplitude-js');
-  amplitude.getInstance().init('b628782272a6f671076be09a7b6fb6b7');
-  amplitude.getInstance().logEvent(event, obj);
+  // amplitude.getInstance().init('b628782272a6f671076be09a7b6fb6b7');
+  // amplitude.getInstance().logEvent(event, obj);
 }
 
 export const setAmplitudeUserId = (userId) => {
   amplitude = require('amplitude-js');
-  amplitude.getInstance().init('b628782272a6f671076be09a7b6fb6b7');
-  amplitude.getInstance().setUserId(`${userId}`);
+  // amplitude.getInstance().init('b628782272a6f671076be09a7b6fb6b7');
+  // amplitude.getInstance().setUserId(`${userId}`);
 }
