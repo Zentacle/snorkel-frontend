1029ae5189de3d288254b5b1e783add284fbb013
Mayank Jain <mjmayank@gmail.com>
2023-02-03 09:26:08 -0500

    Add clearbit tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -40,6 +40,11 @@ export default class MyDocument extends Document {
             key="google-tag-manager"
           />
           <script
+            async
+            src="https://tag.clearbitscripts.com/v1/pk_9f5d0fa8115ed5d8b313be0b7389ede5/tags.js"
+            referrerpolicy="strict-origin-when-cross-origin"
+          />
+          <script
             dangerouslySetInnerHTML={{
               __html: `
                   window.dataLayer = window.dataLayer || [];
