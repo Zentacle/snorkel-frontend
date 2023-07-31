695cffbb36589617a988d63965a50e4f322b4631
Mayank Jain <mjmayank@gmail.com>
2023-07-30 21:01:40 -0400

    Update beach reviews linting

--- src/components/BeachPage/BeachReviews/BeachReviews.js
+++ src/components/BeachPage/BeachReviews/BeachReviews.js
@@ -1,68 +1,73 @@
-import React from "react";
+import React from 'react';
 import Link from 'next/link';
 
-import styles from "../BeachReviews/BeachReviews.module.css";
-import IndividualReview from "./IndividualReview/IndividualReview";
+import styles from '../BeachReviews/BeachReviews.module.css';
+import IndividualReview from './IndividualReview/IndividualReview';
 import { useCurrentUser } from 'context/usercontext';
 import PrimaryButton, { PrimaryLink } from 'components/PrimaryButton';
 import { sendEvent } from 'hooks/amplitude';
 
 const BeachReviews = (props) => {
-    const { beachid, url } = props;
-    const [isCollapsed, setIsCollapse] = React.useState(true);
+  const { beachid, url } = props;
+  const [isCollapsed, setIsCollapse] = React.useState(true);
 
-    const onReviewClick = () => {
-        sendEvent('review_begin', {
-            'site_id': beachid,
-        })
-    }
+  const onReviewClick = () => {
+    sendEvent('review_begin', {
+      site_id: beachid,
+    });
+  };
 
-    const { state } = useCurrentUser();
-    const link = state.user && state.user.id
-        ? `${url}/review`
-        : '/login'
+  const { state } = useCurrentUser();
+  const link = state.user && state.user.id ? `${url}/review` : '/login';
 
-    const onViewAllClick = () => {
-        setIsCollapse(false);
-        sendEvent('click__view_all_reviews')
-    }
+  const onViewAllClick = () => {
+    setIsCollapse(false);
+    sendEvent('click__view_all_reviews');
+  };
 
-    return (
-        <div className={styles.reviewContainer}>
-            <div className={styles.reviewbuttoncontainer}>
-                <PrimaryLink
-                    onClick={onReviewClick}
-                    className={styles.reviewbutton}
-                    href={link}
-                >
-                    Write a Review
-                </PrimaryLink>
-            </div>
-            {props.isSingularReview
-                && <div>
-                    Currently viewing a specific review.&nbsp;
-                    <Link href={url}>
-                        <a className={styles.viewAll}>
-                            View all reviews
-                        </a>
-                    </Link>
-                </div>
-            }
-            <div className={`${styles.reviewsContainer} ${isCollapsed && styles.isCollapsed}`}>
-                {props.reviews && props.reviews.length
-                    ? props.reviews.map((review) =>
-                        <IndividualReview
-                            key={review.id}
-                            review={review}
-                            user={review.user}
-                        />
-                    )
-                    : <div className={styles.emptyState}>No reviews yet. Be the first!</div>
-                }
-            </div>
-            {isCollapsed && props.reviews.length && <PrimaryButton onClick={onViewAllClick}>View all {props.reviews.length} reviews</PrimaryButton>}
+  return (
+    <div className={styles.reviewContainer}>
+      <div className={styles.reviewbuttoncontainer}>
+        <PrimaryLink
+          onClick={onReviewClick}
+          className={styles.reviewbutton}
+          href={`https://zentacle.app.link/Log?beach_id=${beachid}&$deeplink_path=Log?beach_id=${beachid}&utm_medium=xpromo&utm_source=xpromo&campaign=review`}
+        >
+          Write a Review
+        </PrimaryLink>
+      </div>
+      {props.isSingularReview && (
+        <div>
+          Currently viewing a specific review.&nbsp;
+          <Link href={url}>
+            <a className={styles.viewAll}>View all reviews</a>
+          </Link>
         </div>
-    )
-}
+      )}
+      <div
+        className={`${styles.reviewsContainer} ${
+          isCollapsed && styles.isCollapsed
+        }`}
+      >
+        {props.reviews && props.reviews.length ? (
+          props.reviews.map((review) => (
+            <IndividualReview
+              key={review.id}
+              review={review}
+              user={review.user}
+            />
+          ))
+        ) : (
+          <div className={styles.emptyState}>No reviews yet. Be the first!</div>
+        )}
+      </div>
+      {isCollapsed && props.reviews.length && (
+        <PrimaryButton onClick={onViewAllClick}>
+          View all {props.reviews.length} reviews
+        </PrimaryButton>
+      )}
+    </div>
+  );
+};
 
 export default BeachReviews;
